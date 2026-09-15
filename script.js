f8e3213d9b2414dd0f972345ebf709a23be720be
whatsapp-video <dorax.bossx@gmail.com>
2026-09-15 20:57:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     const profiles = [
         {
             image: "assets/profiles/profile1.jpg",
-            name: "DivyaShree",
+            name: "Divya",
             bio: "I'm Hotter than your ex 💋",
             place: "Chennai",
             age: 26,
